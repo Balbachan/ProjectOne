55bf73d8f2ff6a5f8b83c01f0bc2a30a34890038
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2023-07-26 14:45:37 -0300

    Initial Commit

---
+++ ProjectOne/ProjectOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOneApp.swift
+//  ProjectOne
+//
+//  Created by Laura C. Balbachan dos Santos on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
